f4398b62f93679ab199df93c3c2b1320ec8e30a7
gaha4495 <gaha4495@colorado.edu>
2022-04-20 13:22:40 -0600

    Added Return Form Container and Modal Pop-up functionality

---
+++ src/resources/js/script.js
@@ -0,0 +1,10 @@
+function submitAudio(){
+    var form = document.getElementById("audioForm");
+    var submission = document.getElementById("audioSelection").val();
+    var button = document.getElementById("audioSubmit");
+
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
--- src/server.js
+++ src/server.js
@@ -64,38 +64,7 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
 
  Web Page Requests:
 
-  Login Page:        Provided For your (can ignore this page)
-  Registration Page: Provided For your (can ignore this page)
-  Home Page:
-  		/home - get request (no parameters)
-  				This route will make a single query to the favorite_colors table to retrieve all of the rows of colors
-  				This data will be passed to the home view (pages/home)
-
-  		/home/pick_color - post request (color_message)
-  				This route will be used for reading in a post request from the user which provides the color message for the default color.
-  				We'll be "hard-coding" this to only work with the Default Color Button, which will pass in a color of #FFFFFF (white).
-  				The parameter, color_message, will tell us what message to display for our default color selection.
-  				This route will then render the home page's view (pages/home)
-
-  		/home/pick_color - get request (color)
-  				This route will read in a get request which provides the color (in hex) that the user has selected from the home page.
-  				Next, it will need to handle multiple postgres queries which will:
-  					1. Retrieve all of the color options from the favorite_colors table (same as /home)
-  					2. Retrieve the specific color message for the chosen color
-  				The results for these combined queries will then be passed to the home view (pages/home)
-
-  		/team_stats - get request (no parameters)
-  			This route will require no parameters.  It will require 3 postgres queries which will:
-  				1. Retrieve all of the football games in the Fall 2018 Season
-  				2. Count the number of winning games in the Fall 2018 Season
-  				3. Count the number of lossing games in the Fall 2018 Season
-  			The three query results will then be passed onto the team_stats view (pages/team_stats).
-  			The team_stats view will display all fo the football games for the season, show who won each game,
-  			and show the total number of wins/losses for the season.
-
-  		/player_info - get request (no parameters)
-  			This route will handle a single query to the football_players table which will retrieve the id & name for all of the football players.
-  			Next it will pass this result to the player_info view (pages/player_info), which will use the ids & names to populate the select tag for a form
+  
 ************************************/
 
 //Main page
@@ -134,6 +103,24 @@ app.get('/', function(req, res) {
 		})
 });
 
+/* +===== AJAX =====+ */ 
+$(document).ready(function() {
+  let endpoint = " theaudiodb.com/api/v1/json/2/search.php?s="
+
+  $( ".content a" ).each(function( index, element ) {
+    
+    $.ajax({
+        url: endpoint + $(this).text(),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+            console.log(result);
+        }
+    })
+  });
+});
+
+/* +===== TESTING =====+ */
 
 //app.listen(3000);
 const server = app.listen(process.env.PORT || 3000, () => {
