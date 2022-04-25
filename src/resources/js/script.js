c1ad4368cfe90cc4f0c7fc3e916188265791a0e5
gaha4495 <gaha4495@colorado.edu>
2022-04-25 16:17:43 -0600

    Added Search history functionality + attempted testing

--- src/resources/js/script.js
+++ src/resources/js/script.js
@@ -1,10 +1,82 @@
+/* +===== AJAX =====+ */
+
+//GET audio
+//Note: If the API returns more than one result, 
+//render the first result. If any of the data points 
+//do not have the expected data, 
+//label those field values as '-' or 'Link unavailable'
 function submitAudio(){
     var form = document.getElementById("audioForm");
-    var submission = document.getElementById("audioSelection").val();
+    var submission = document.getElementById("audioSelection").value;
     var button = document.getElementById("audioSubmit");
+    console.log(submission);
+    var url = `https://cors-anywhere.herokuapp.com/https://theaudiodb.com/api/v1/json/2/search.php?s=${submission}`
+    console.log("Attempting to retrieve submitted audio");
+    $.ajax({
+        headers: {
+            "accept": "application/json",
+            "Access-Control-Allow-Origin":"*"
+        },
+        url: url,
+        cors: true,
+        crossDomain: true,
+        contentType: "application/json",
+        dataType: 'json',
+        type: 'GET',
+    }).then(data => {
+        console.log("Ajax called")
+        console.log(data);
 
+        //Grab Data from API
+        var nameAlt = data.artists[0].strArtistAlternate;
+        var genre = data.artists[0].strGenre;
+        var country = data.artists[0].strCountry;
+        var bio = data.artists[0].strBiographyEN;
+        var label = data.artists[0].strLabel;
+
+        //Print to Page
+        document.getElementById("li1").innerHTML = nameAlt;
+        document.getElementById("li2").innerHTML = genre;
+        document.getElementById("li3").innerHTML = country;
+        document.getElementById("li4").innerHTML = bio;
+        document.getElementById("li5").innerHTML = label;
+        document.getElementById("modalSubmission").innerHTML = submission;
+    })
+    .catch(err =>{
+        console.log(err);
+    })
 }
 
+//SAVE audio
+function saveAudio(){
+    console.log("Attempting to Save Audio");
+    //Grab Data from Front End
+    const submission = document.getElementById("modalSubmission").innerHTML;
+    const nameAlt = document.getElementById("li1").innerHTML;
+    const genre = document.getElementById("li2").innerHTML;
+    const country = document.getElementById("li3").innerHTML;
+    const bio = document.getElementById("li4").innerHTML;
+    const label = document.getElementById("li5").innerHTML;
+
+    var dbData = {submission: submission, nameAlt: nameAlt, genre: genre, country: country, bio: bio, label: label};
+    console.log(dbData);
+    $.ajax({
+        url: "/searches",
+        type: 'POST',
+        //dataType:"json",
+        data: dbData,
+    }).then(data => {
+        alert("Submission Saved to Search History!");
+    }).catch(err =>{
+        console.log(err);
+    });
+}
+ 
+function retrieveAudio(){
+    var query = `SELECT * FROM savedAudio`;
+}
+/* +===== MODAL =====+ */
 $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+    $('#myInput').trigger('focus')    
+})
+
--- src/server.js
+++ src/server.js
@@ -17,22 +17,22 @@ var pgp = require('pg-promise')();
 /**********************
   Database Connection information
   host: This defines the ip address of the server hosting our database.
-		We'll be using `db` as this is the name of the postgres container in our
-		docker-compose.yml file. Docker will translate this into the actual ip of the
-		container for us (i.e. can't be access via the Internet).
+    We'll be using `db` as this is the name of the postgres container in our
+    docker-compose.yml file. Docker will translate this into the actual ip of the
+    container for us (i.e. can't be access via the Internet).
   port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
   database: This is the name of our specific database.  From our previous lab,
-		we created the football_db database, which holds our football data tables
+    we created the football_db database, which holds our football data tables
   user: This should be left as postgres, the default user account created when PostgreSQL was installed
   password: This the password for accessing the database. We set this in the
-		docker-compose.yml for now, usually that'd be in a seperate file so you're not pushing your credentials to GitHub :).
+    docker-compose.yml for now, usually that'd be in a seperate file so you're not pushing your credentials to GitHub :).
 **********************/
 const dev_dbConfig = {
-	host: 'db',
-	port: 3000,
-	database: process.env.POSTGRES_DB,
-	user:  process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD
 };
 
 /** If we're running in production mode (on heroku), the we use DATABASE_URL
@@ -44,7 +44,7 @@ const dbConfig = isProduction ? process.env.DATABASE_URL : dev_dbConfig;
 // Heroku Postgres patch for v10
 // fixes: https://github.com/vitaly-t/pg-promise/issues/711
 if (isProduction) {
-  pgp.pg.defaults.ssl = {rejectUnauthorized: false};
+  pgp.pg.defaults.ssl = { rejectUnauthorized: false };
 }
 
 const db = pgp(dbConfig);
@@ -68,61 +68,114 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
 ************************************/
 
 //Main page
-app.get('/', function(req, res) {
-	res.render('pages/main',{
+app.get('/', function (req, res) {
+  res.render('pages/main', {
     my_title: "Main Audio API Page"
-	});
-});
-
-// Search History page
-app.get('/searches', function(req, res) {
-	res.render('pages/searches',{
-		my_title:"Search History Page"
-	});
-});
-
-/*Add your other get/post request handlers below here: */
-
-app.get('/', function(req, res) {
-	var query = '';
-	db.any(query)
-		.then(function (rows) {
-			res.render('pages/main',{
-				my_title: "Main Audio API Page",
-				data: rows,
-			})
-
-		})
-		.catch(function (err) {
-			// display error message in case an error
-			request.flash('error', err);
-			response.render('pages/main', {
-				my_title: "Main Audio API Page",
-				data: '',
-			})
-		})
-});
-
-/* +===== AJAX =====+ */ 
-$(document).ready(function() {
-  let endpoint = " theaudiodb.com/api/v1/json/2/search.php?s="
-
-  $( ".content a" ).each(function( index, element ) {
-    
-    $.ajax({
-        url: endpoint + $(this).text(),
-        contentType: "application/json",
-        dataType: 'json',
-        success: function(result){
-            console.log(result);
-        }
-    })
   });
 });
 
-/* +===== TESTING =====+ */
-
-//app.listen(3000);
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+//Update Search History Table
+app.get('/searches', function (req, res) {
+  var Artists = 'SELECT * FROM savedAudio';
+  console.log(Artists);
+  db.task('get-everything', task => {
+    return task.batch([
+      task.any(Artists)
+    ]);
+  }).then(data => {
+    res.render('pages/searches', {
+      my_title: "Search History Page",
+      Artists: data[0],
+    })
+  })
+    .catch(function (err) {
+      // display error message in case an error
+      request.flash('error', err);
+      response.render('pages/main', {
+        my_title: "Search History Page",
+        data: '',
+      })
+    });
 });
+
+  //SUBMIT AUDIO
+  app.post('/searches', function (req, res) {
+    //Save val's from page
+    const submission = req.body.submission;
+    const nameAlt = req.body.nameAlt;
+    const genre = req.body.genre;
+    const country = req.body.country;
+    var bio = req.body.bio;
+    const label = req.body.label;
+
+    bio = bio.replace(/'/g, "");
+    bio = bio.replace(/"/g, "");
+    bio = bio.replace(/(\r\n|\n|\r)/gm, "");
+
+    //Query to insert val's
+    var query = `Insert into savedAudio (submission,nameAlt,genre,country,bio,label) values('${submission}','${nameAlt}','${genre}','${country}','${bio}','${label}')`;
+
+    console.log(query.slice(390, 400));
+
+    //Insert command into savedAudio_db
+    db.query(query)
+      .then(function (rows) {
+        res.status(200);
+      })
+      .catch(function (err) {
+        console.log('error', err);
+        res.status(500);
+      });
+  });
+
+  /* +===== TESTING =====+ */
+  //docker-compose run web npm test
+  //Might need to move test folder into src (note .yml volumes)
+  //docker-compose build
+  //module.exports = app.listen(3000);
+
+  const testArtists = [
+    {
+    id: 1,
+    submission: "Eminem" ,       /* Stage Name of the Submitted Artist / Group                     */
+    nameAlt: "Marshall Mathers III" ,   /*     Real Name     */
+    genre: "Hip-Hop",  /* Artist Genre */
+    country: "USA",        /* Place of Birt    */
+    bio: "A LONG ASS BIOGRAPHY",         /* Small Artist BIO*/
+    label: "AfterMath Records",
+    },
+    {
+    id: 2,
+    submission: "Madonna" ,       /* Stage Name of the Submitted Artist / Group                     */
+    nameAlt: "Madonna Louise Vernica Ciccone" ,   /*     Real Name     */
+    genre: "Pop",  /* Artist Genre */
+    country: "Michigan, USA",        /* Place of Birt    */
+    bio: "A LONG ASS BIOGRAPHY",         /* Small Artist BIO*/
+    label: "Interscope Records",
+    }
+  ]
+
+// Simple get api provided to check if the node.js starts up successfully. Opening up http://localhost:3000 should display the below returned json.
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "Welcome!" });
+});
+
+app.get("/searches", (request, response) => {
+  response.send(ops);
+});
+
+// GET (BY ID)
+app.get("/searches/:id", (request, response) => {
+  const artistId = request.params.id;
+  const artist = testArtists.find((artist) => artist.id === parseInt(artistId));
+  if (!artist)
+    return response
+      .status(404)
+      .send("The artist with the provided ID does not exist.");
+  response.send(op);
+});
+
+  //app.listen(3000);
+  const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+  });
